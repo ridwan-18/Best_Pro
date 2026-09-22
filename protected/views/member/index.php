b4cb5cf12ecdf7c9468454dfaf40b6edee12a3bc
ridwan-18 <ridwannurasyid23@gmail.com>
2026-09-22 18:37:40 +0700

    commit

--- protected/views/member/index.php
+++ protected/views/member/index.php
@@ -90,9 +90,8 @@ $this->title = 'Member - ' . Yii::$app->name;
                                 <th>Created At</th>
                                 <th width="1">Action</th>
 								   <th>Total Member</th>
-                              <?php if ((int)$user->id === 1) { ?>
 								 <th>Upload Invoice</th>
-								 		<?php } ?>
+								 		
                             </tr>
                         </thead>
                         <tbody>
